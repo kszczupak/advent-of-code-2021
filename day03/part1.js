d15210426837d9a209040c6afdcce2eaea4d5170
Krzysztof Szczupak <kszczupak00@gmail.com>
2021-12-03 20:41:42 +0100

    added solutions for day 3

---
+++ day03/part1.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+
+const rawData = fs.readFileSync("./input.txt", "utf8")
+// const rawData = fs.readFileSync("./example.txt", "utf8")
+const parsedData = rawData.split("\r\n");
+
+let sumBits = Array(parsedData[0].length).fill(0);
+
+
+parsedData.forEach(values => {
+    values.split("").forEach((rawBit, idx) => {
+        sumBits[idx] += parseInt(rawBit);
+    })
+})
+
+const gammaRateBits = [];
+const epsilonRateBits = [];
+
+sumBits.forEach(sum => {
+    gammaRateBits.push((sum > parsedData.length/2) ? "1" : "0");
+    epsilonRateBits.push((sum < parsedData.length/2) ? "1" : "0");
+})
+
+const gammaRate = parseInt(gammaRateBits.join(""), 2)
+const epsilonRate = parseInt(epsilonRateBits.join(""), 2)
+console.log(gammaRate * epsilonRate)
---
+++ day03/part2.js
@@ -0,0 +1,42 @@
+const fs = require("fs")
+
+const rawData = fs.readFileSync("./input.txt", "utf8")
+// const rawData = fs.readFileSync("./example.txt", "utf8")
+const parsedData = rawData.split("\r\n");
+
+const numberLength = parsedData[0].length;
+
+function getSumBit(allBits, idx) {
+    let sumBits = Array(parsedData[0].length).fill(0);
+
+    allBits.forEach(values => {
+        values.split("").forEach((rawBit, j) => {
+            sumBits[j] += parseInt(rawBit);
+        })
+    })
+
+    return sumBits[idx]
+}
+
+
+function findRating(condition) {
+    let candidates = [...parsedData]
+
+    for (let idx = 0; idx < numberLength; idx++) {
+        const sumBit = getSumBit(candidates, idx)
+        const searchBit = condition(sumBit, candidates.length) ? "1" : "0"
+
+        let newCandidates = [];
+        for (let candidate of candidates) {
+            if (candidate[idx] === searchBit) newCandidates.push(candidate);
+        }
+        if (newCandidates.length === 1) return newCandidates[0];
+
+        candidates = newCandidates;
+    }
+}
+
+const oxygenRating = parseInt(findRating((x, y) => x >= y/2), 2)
+const co2Rating = parseInt(findRating((x, y) => x < y/2), 2)
+
+console.log(oxygenRating * co2Rating)
