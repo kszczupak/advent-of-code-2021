2eba9056fef63dff0d5332f05295f4e18e559dce
Krzysztof Szczupak <kszczupak00@gmail.com>
2021-12-04 23:41:35 +0100

    added solution for day 4

---
+++ day04/part1.js
@@ -0,0 +1,83 @@
+const fs = require("fs")
+
+const rawData = fs.readFileSync("./input.txt", "utf8").split("\r\n");
+// const rawData = fs.readFileSync("./example.txt", "utf8").split("\r\n");
+const drawnNumbers = rawData[0].split(",").map(x => parseInt(x));
+
+class BingoBoard {
+    constructor(rawBoard) {
+        this.size = rawBoard.length;
+        this.allElements = [...Array(this.size * this.size)]
+        this.rows = [...Array(this.size)].map(e => new Set())
+        this.columns = [...Array(this.size)].map(e => new Set())
+        this.bingo = false
+        this.marked = new Set()
+
+
+        this.#parseRawBoard(rawBoard)
+    }
+
+    mark(number) {
+        this.rows.forEach(row => {
+            row.delete(number)
+            if (row.size === 0) this.bingo = true
+        })
+
+        this.columns.forEach(column => {
+            column.delete(number)
+            if (column.size === 0) this.bingo = true
+        })
+
+        this.marked.add(number)
+    }
+
+    score(number){
+        let sum = 0
+        this.allElements.forEach(element => {
+            if (!this.marked.has(element)) sum += element
+        })
+
+        return number * sum
+    }
+
+    #parseRawBoard(rawBoard) {
+        for (let [rowIdx, rawRow] of rawBoard.entries()) {
+            let colIdx = 0
+            for (let rawElement of rawRow.split(" ")) {
+                if (rawElement === "") continue
+                const element = parseInt(rawElement)
+
+                this.columns[colIdx].add(element)
+                this.rows[rowIdx].add(element)
+                this.allElements[rowIdx * this.size + colIdx] = element
+                colIdx++
+            }
+        }
+    }
+}
+
+const boards = [];
+let rawBoard = []
+
+for (let row of rawData.slice(2)) {
+    if (row === "") {
+        const board = new BingoBoard(rawBoard)
+        boards.push(board)
+        rawBoard = []
+        continue
+    }
+    rawBoard.push(row)
+}
+
+function simulateGame(boards, numbers) {
+    for (let draw of numbers) {
+        for (let board of boards) {
+            board.mark(draw)
+            if (board.bingo) return [board, draw]
+        }
+    }
+}
+
+const [winningBoard, winingNumber] = simulateGame(boards, drawnNumbers)
+
+console.log(winningBoard.score(winingNumber))
